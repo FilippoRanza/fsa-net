775ad17185f3cd25a3d4dbcb9d473ec25e91c518
FilippoRanza <filipporanza@gmail.com>
2021-02-11 20:26:07 +0100

    Add test syntax functions, but no test code :grin::grin:

--- fsa-net-parser/src/lib.rs
+++ fsa-net-parser/src/lib.rs
@@ -6,8 +6,37 @@ mod syntax_tree;
 
 #[cfg(test)]
 mod tests {
+
+    use std::path::PathBuf;
+    use std::fs::File;
+    use std::io::Read;
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_syntax() {
+        
+        let base_dir = PathBuf::from("fnl-test-code");
+        assert!(base_dir.is_dir());
+        for entry in base_dir.read_dir().expect("Cannot read fnl-test-code") {  
+            let file = entry.unwrap().path();
+            let is_ok = try_syntax(&file).unwrap();
+            assert!(is_ok);
+        }
     }
+
+    fn try_syntax(file: &PathBuf) -> Result<bool, std::io::Error> {
+        let code = load_file(file)?;
+        let parser = fsa_net_lang::FsacodeParser::new();
+        let result = parser.parse(&code);
+        Ok(result.is_ok())
+    }
+
+
+    fn load_file(path: &PathBuf) -> Result<String, std::io::Error> {
+        let mut file = File::open(path)?;
+        let mut code = String::new();
+        file.read_to_string(&mut code)?;
+        Ok(code)
+    }
+
 }
